5f45f21363b4140415b58f673f0cc93dc7d103a3
sem <steventbet@gmail.com>
2018-01-08 20:05:08 +0800

    redux 及 路由架子搭建
    redux 及 路由架子搭建

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Routes from './route';
+import { createStore,applyMiddleware,compose } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './redux/reducer';
+import thunk from 'redux-thunk';
+// 组织 redux 中间件 
+const store = createStore(reducer,compose(
+    applyMiddleware(thunk)
+));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <Routes />
+    </Provider>
+    , document.getElementById('root'));
---
+++ src/route/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { Router, Route, IndexRoute, Redirect, browserHistory } from 'react-router';
+
+//  路由配置
+class Route extends Component{
+    
+} 
